33c48d8cd397c1550ca0a2324dcec470eaef33b8
Lukas Herzog <git@lukasherzog.de>
2021-03-29 18:57:40 +0200

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chocolatey Packages for Community Repo
+
+### Currently supported packages
+
+-   Ultraschall 5 (High End Podcasting for REAPER)
+
+### TODO
+
+-   Add Release Notes and Summary to Ultraschall Package
---
+++ ultraschall/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'Ultraschall-5.0.2.msi'
+
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI' #only one of these: exe, msi, msu
+  file           = $fileLocation
+
+  softwareName   = 'ultraschall*' 
+  checksum       = 'F8EB295E4C816F0A52029C9398C50C700430BCBEEE28EB10793E74B566E14703'
+  checksumType   = 'sha256'
+  # MSI
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
